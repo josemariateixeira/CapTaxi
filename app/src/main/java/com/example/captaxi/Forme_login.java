a0aa99db2d7e850ef506337114f0436247f2694b
José Maria <jom.silva22@gmail.com>
2022-10-07 01:02:46 -0300

    trabalho ate meia noite dia 07/10

---
+++ app/src/main/java/com/example/captaxi/Forme_login.java
@@ -0,0 +1,14 @@
+package com.example.captaxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Forme_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forme_login);
+    }
+}
