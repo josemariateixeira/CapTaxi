5751ff92ab3948ef82c0b09c039f8e5355472065
Lord-Souza <115049807+Everton-Knowbi@users.noreply.github.com>
2022-10-07 23:15:38 -0300

    Update Forme_login.java

--- app/src/main/java/com/example/captaxi/Forme_login.java
+++ app/src/main/java/com/example/captaxi/Forme_login.java
@@ -2,13 +2,32 @@ package com.example.captaxi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Forme_login extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forme_login);
+
+        getSupportActionBar().hide();
+        IniciarConmponentes();
+
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Forme_login.this,Form_cadastro.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void IniciarConmponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
     }
 }
