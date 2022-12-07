4465fa023969c0cab791423f210bc3e9f684c539
Lord-Souza <104236211+Lord-Souza@users.noreply.github.com>
2022-12-06 23:18:12 -0300

    Create PerfilCadastro.java

---
+++ app/src/main/java/com/example/captaxi/PerfilCadastro.java
@@ -0,0 +1,32 @@
+package com.example.captaxi;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class PerfilCadastro extends Fragment {
+
+
+
+    public PerfilCadastro() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_perfil_cadastro, container, false);
+    }
+}
